a5cffb3eaef8378fea3aac37b9b3f2651844ea4a
miancho <miancho@gmail.com>
2023-08-01 00:05:06 -0300

    nixt-windcss

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="bg-blue-600 font-medium px-4 rounded-xl text-white">
+    <button class="bg-red-600 font-medium px-4 rounded-xl text-white">
         <slot />
     </button>
 </template>
